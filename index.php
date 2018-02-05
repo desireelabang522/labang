5aaddf63b082ae5b25c37a19aa7ddac8a13ce41a
desireelabang522 <36143986+desireelabang522@users.noreply.github.com>
2018-02-05 15:35:43 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+
+<title>WEB DESIGN 1</title>
+</head>
+<body style="background-color:pink;">
+
+<center><h1>PERSONAL DATA</h1></center>
+<p><h2>NAME:<u>Labang, Desiree P.</u></h2></p>
+<p><h2>SEX:<u>Female</u></h2></p>
+<p><h2>ADDRESS:<u>Uling City of Naga Cebu</u></h2></p>
+<p><h2>DATE OF BIRTH:<u>December 7 1987</u></h2><p>
+<p><h2>CIVIL STATUS:<u>Single</u></h2></p>
+<p><h2>RELIGION:<u>Roman Catholic</u></h2></p>
+<p><h2>CITIZENSHIP:<u>Filipino</u></h2></p>
+<p><h2>EMAIL ADDRESS:<u>labangdesiree@gmail.com</u></h2></p><hr>
+<center><h1>EDUCATIONAL ATTAINMENT</h1></center>
+<p><h2>ELEMENTARY:<u>Uling Elementary School</u></h2></p>
+<p><h2>HIGH SCHOOL:<u>Naga National High School</u></h2></p>
+<p><h2>COLLEGE:<u>Cebu Technological University</u></h2></p><hr>
+<center><h1>BIOGRAPHY</h1></center>
+<p><h2>
+<center><i>I'm a silent and moody person!!</i></center>
+I always put in my mind that family first that is why after 
+I finish my studies I find a job so that i can help my family.
+For 10 years worked i realize that my life don't have any improvement
+because im lack of education. So i decided to enroll in college not only for my future 
+but also to my healthy and energetic 8 months old baby boy.
+He is my inspiration for my studies.
+</h2></p>
+</body>
+</html>
